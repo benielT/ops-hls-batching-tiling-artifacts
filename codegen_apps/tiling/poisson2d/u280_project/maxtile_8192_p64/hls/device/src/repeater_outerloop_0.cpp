5150f3d8f44b493015287f4f98feb751f995d5ee
Beniel Thileepan <Beniel.Thileepan@warwick.ac.uk>
2026-08-01 23:46:16 +0200

    Tiling poisson2d maxtile_8192_p64 added

---
+++ codegen_apps/tiling/poisson2d/u280_project/maxtile_8192_p64/hls/device/src/repeater_outerloop_0.cpp
@@ -0,0 +1,20 @@
+// Auto-generated at 2026-08-01 02:40:45.164517 by ops-translator
+
+#pragma once
+#include <ops_hls_kernel_support.h>
+#include "../../common/include/common_config.hpp"
+
+extern "C" void repeater_outerloop_0(
+    //u
+        hls::stream <ap_axiu<axis_data_width, 0, 0, 0>>& arg0_axis_in,
+    //u2
+        hls::stream <ap_axiu<axis_data_width, 0, 0, 0>>& arg1_axis_out
+)
+{
+    #pragma HLS INTERFACE axis port = arg0_axis_in register     #pragma HLS INTERFACE axis port = arg1_axis_out register 
+    #pragma HLS INTERFACE ap_ctrl_none port=return
+    
+    #pragma HLS PIPELINE II=1 style=flp
+    
+    arg1_axis_out.write(arg0_axis_in.read());
+}
---
+++ codegen_apps/tiling/poisson2d/u280_project/maxtile_8192_p64/hls/host/kernel_wrappers/hls_kernels.hpp
@@ -0,0 +1,24 @@
+// Auto-generated at 2026-08-01 02:40:43.249352 by ops-translator
+
+#pragma once
+
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <ops_hls_rt_support.h>
+
+extern float dx;
+extern float dy;
+extern float dy_2;
+extern float dx_2;
+extern float dx_2_plus_dy_2_mult_2;
+extern float dx_2_dy_2;
+
+// user kernel files
+#include "poisson_kernel_populate_kernel.hpp"
+#include "poisson_kernel_update_kernel.hpp"
+#include "poisson_kernel_initialguess_kernel.hpp"
+#include "outerloop_0.hpp"
+
