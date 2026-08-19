7227fbb751c13b56175d8464e4e22b69ed6b9cae
Beniel Thileepan <thileepanbeniel@gmail.com>
2026-08-19 04:35:37 +0300

    adding poisson2d aligned interleave p7 app with results

---
+++ codegen_apps/tiling/poisson2d/u280_project/maxtile_8192_align_interleave_p7/hls/device/src/repeater_outerloop_0.cpp
@@ -0,0 +1,41 @@
+// Auto-generated at 2026-08-18 17:01:51.389317 by ops-translator
+
+#pragma once
+#include <ops_hls_kernel_support.h>
+#include "../../common/include/common_config.hpp"
+
+extern "C" void repeater_outerloop_0(
+    //u
+        hls::stream <ap_axiu<axis_data_width, 0, 0, 0>>& arg0_0_axis_in,
+        hls::stream <ap_axiu<axis_data_width, 0, 0, 0>>& arg0_1_axis_in,
+        hls::stream <ap_axiu<axis_data_width, 0, 0, 0>>& arg0_2_axis_in,
+        hls::stream <ap_axiu<axis_data_width, 0, 0, 0>>& arg0_3_axis_in,
+        hls::stream <ap_axiu<axis_data_width, 0, 0, 0>>& arg0_4_axis_in,
+        hls::stream <ap_axiu<axis_data_width, 0, 0, 0>>& arg0_5_axis_in,
+        hls::stream <ap_axiu<axis_data_width, 0, 0, 0>>& arg0_6_axis_in,
+        hls::stream <ap_axiu<axis_data_width, 0, 0, 0>>& arg0_7_axis_in,
+    //u2
+        hls::stream <ap_axiu<axis_data_width, 0, 0, 0>>& arg1_0_axis_out,
+        hls::stream <ap_axiu<axis_data_width, 0, 0, 0>>& arg1_1_axis_out,
+        hls::stream <ap_axiu<axis_data_width, 0, 0, 0>>& arg1_2_axis_out,
+        hls::stream <ap_axiu<axis_data_width, 0, 0, 0>>& arg1_3_axis_out,
+        hls::stream <ap_axiu<axis_data_width, 0, 0, 0>>& arg1_4_axis_out,
+        hls::stream <ap_axiu<axis_data_width, 0, 0, 0>>& arg1_5_axis_out,
+        hls::stream <ap_axiu<axis_data_width, 0, 0, 0>>& arg1_6_axis_out,
+        hls::stream <ap_axiu<axis_data_width, 0, 0, 0>>& arg1_7_axis_out
+)
+{
+    #pragma HLS INTERFACE axis port = arg0_0_axis_in register     #pragma HLS INTERFACE axis port = arg0_1_axis_in register     #pragma HLS INTERFACE axis port = arg0_2_axis_in register     #pragma HLS INTERFACE axis port = arg0_3_axis_in register     #pragma HLS INTERFACE axis port = arg0_4_axis_in register     #pragma HLS INTERFACE axis port = arg0_5_axis_in register     #pragma HLS INTERFACE axis port = arg0_6_axis_in register     #pragma HLS INTERFACE axis port = arg0_7_axis_in register     #pragma HLS INTERFACE axis port = arg1_0_axis_out register     #pragma HLS INTERFACE axis port = arg1_1_axis_out register     #pragma HLS INTERFACE axis port = arg1_2_axis_out register     #pragma HLS INTERFACE axis port = arg1_3_axis_out register     #pragma HLS INTERFACE axis port = arg1_4_axis_out register     #pragma HLS INTERFACE axis port = arg1_5_axis_out register     #pragma HLS INTERFACE axis port = arg1_6_axis_out register     #pragma HLS INTERFACE axis port = arg1_7_axis_out register 
+    #pragma HLS INTERFACE ap_ctrl_none port=return
+    
+    #pragma HLS PIPELINE II=1 style=flp
+    
+    arg1_0_axis_out.write(arg0_0_axis_in.read());
+    arg1_1_axis_out.write(arg0_1_axis_in.read());
+    arg1_2_axis_out.write(arg0_2_axis_in.read());
+    arg1_3_axis_out.write(arg0_3_axis_in.read());
+    arg1_4_axis_out.write(arg0_4_axis_in.read());
+    arg1_5_axis_out.write(arg0_5_axis_in.read());
+    arg1_6_axis_out.write(arg0_6_axis_in.read());
+    arg1_7_axis_out.write(arg0_7_axis_in.read());
+}
